6aa5b2d4e8aa19b2f0d9dca8147467ded207a98b
Neoterux <neoterux@gmail.com>
2021-07-08 20:14:30 -0500

    Added methods to PaneUtils.kt

--- src/main/kotlin/com/neoterux/jfx/pane/PaneUtils.kt
+++ src/main/kotlin/com/neoterux/jfx/pane/PaneUtils.kt
@@ -1,6 +1,7 @@
 @file:JvmName("PaneUtils")
 package com.neoterux.jfx.pane
 
+import javafx.scene.Node
 import javafx.scene.layout.AnchorPane
 import javafx.scene.layout.Pane
 
@@ -10,3 +11,23 @@ fun Pane.setAnchors(top:Double = 1.0, right: Double = top, bottom: Double = top,
     AnchorPane.setBottomAnchor(this, bottom)
     AnchorPane.setLeftAnchor(this, left)
 }
+
+/**
+ * Add a node to the Pane children hierarchy.
+ */
+fun Pane.addChild(node: Node) = children.add(node)
+
+/**
+ * Add multiple children to the Pane hierarchy.
+ */
+fun Pane.addChildren(vararg nodes: Node) = children.addAll(nodes)
+
+/**
+ * Remove a child from the hierarchy.
+ */
+fun Pane.removeChild(node: Node) = children.remove(node)
+
+/**
+ * Remove multiple children from the hierarchy.
+ */
+fun Pane.removeChildren(vararg nodes: Node) = children.removeAll(nodes)
